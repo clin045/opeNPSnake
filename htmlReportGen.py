0a37ba6b19139ed084aa922a6f90663f0515a4ef
jerloch <jerloch@gmail.com>
2014-06-16 10:03:15 -0500

    Report amount of times something happened

--- htmlReportGen.py
+++ htmlReportGen.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
 import datetime
 
-#hideous code to create the html report file with the prefix in the format of year month day hour minute
-report = open(str(datetime.datetime.today()).replace(':','').replace('.','').replace(' ','').replace('-','')[0:14]+'report.html','w')
-
-
 #this method expects a two-dimensional list containing the data as lst, and a one-dimensional list containing titles as titlelst
-def generate(lst,titlelst):
+def generate(lst,titlelst,count=[]):
     #make the document pretty (inthefuture)
-
+    #hideous code to create the html report file with the prefix in the format of year month day hour minute
+    report = open(str(datetime.datetime.today()).replace(':','').replace('.','').replace(' ','').replace('-','')[0:14]+'report.html','w')
     report.write('<table border="1">')
     report.write('<tr>\n')
     #writes titles as heading
     for title in titlelst:
         report.write('<th>'+title+'</th>\n')
+    report.write('<th>Amount</th>\n')
     report.write('</tr>\n')
     #writes data
     for i in range(0,len(lst)):
         report.write('<tr>\n')
         for j in range(0,len(lst[i])):
             report.write('<td>'+str(lst[i][j])+'</td>\n')
-            
+        if count != []:
+            report.write('<td>'+str(count[i])+'</td>\n')
         report.write('</tr>\n')
     report.write('</table>')
     report.close()
     
     
 #for testing purposes
-#generate([[0,1],[2,3]],['cats','dogs'])
+#generate([[0,1],[2,3]],['cats','dogs'], [10, 12])
 
 
     
