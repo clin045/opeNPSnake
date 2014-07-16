2ff8be4c8c7b95e5d0f2938bc1e798089bf5b4d8
nomadpenguins <christopher.lin045@gmail.com>
2014-07-16 15:48:36 -0500

    Moved global variables to their own class

--- lib/opeNPSnake.py
+++ lib/opeNPSnake.py
@@ -1,18 +1,20 @@
 import os, sys, getopt, configparser, datetime, argparse
 import htmlReportGen, fileParser, helperFunctions
 
-possible_params = []            #Possible parameters they could input
-parameters = {}                 #Dictionary that holds the parameters:their filters
 
-outputDir, inputDir = '', ''    #Folder where to report, folder where logs are
+class g: #Class to contain all the global variables. Not sure if it's best practice, but I saw it in a project once and thought it was a good idea.
+	possible_params = []            #Possible parameters they could input
+	parameters = {}                 #Dictionary that holds the parameters:their filters
 
-values, count = [], []          #Values holds the lines in the report
+	outputDir, inputDir = '', ''    #Folder where to report, folder where logs are
+
+	values, count = [], []          #Values holds the lines in the report
                                 #Count is how many times an identical line appeared
 
 
-start_time, end_time = "", ""   #Start and end time frames given by user
+	start_time, end_time = "", ""   #Start and end time frames given by user
 
-outputFormat = 'html'
+	outputFormat = 'html'
 
 #When -h is called or the user doesn't do something right
 helpfile="""
@@ -28,14 +30,13 @@ python opeNPSnake -i "C:\\Users\\user.name\\Desktop\\NPSLogFile\\weekend" -p "Fu
 #Checks the parameters that the user specified and drops the ones that
 #Aren't in the logs
 def getParameters(params):
-    possible_params = fileParser.checkFilesForParameters(inputDir)
-    global parameters
+    g.possible_params = fileParser.checkFilesForParameters(g.inputDir)
     for p in params:
         if p[0] == " ":
             p = p[1:]
-        if p not in parameters and p in possible_params:
-            parameters[p] = params[p]
-        elif p not in possible_params:
+        if p not in g.parameters and p in g.possible_params:
+            g.parameters[p] = params[p]
+        elif p not in g.possible_params:
             print(p + " is not a valid parameter")
 
 #Returns a dictionary {key:value} = {parameter:[filter1, filter2, etc]}            
@@ -56,7 +57,6 @@ def getFilters(arg):
 
 #loads config file
 def loadConf(loc):
-    global inputDir,outputDir,outputFormat
     loc = loc.replace('\\', '/')
     c = configparser.ConfigParser()
     c.read(loc)
@@ -68,32 +68,27 @@ def loadConf(loc):
             arg = c.get(section,o)
             paramlst = getFilters(arg)
         if o == 'input':
-            inputDir = helperFunctions.getFolderPath(c.get(section,o))
+            g.inputDir = helperFunctions.getFolderPath(c.get(section,o))
         if o == 'output':
-            outputDir = helperFunctions.getFolderPath(c.get(section,o))
+            g.outputDir = helperFunctions.getFolderPath(c.get(section,o))
         if o == 'timeframe':
-            parameters['Timestamp']=''
+            g.parameters['Timestamp']=''
             arg = c.get(section,o)
-            global start_time,end_time
-            start_time,end_time=arg.split(',')[0].split(' '),arg.split(',')[1].split(' ')
+            g.start_time,g.end_time=arg.split(',')[0].split(' '),arg.split(',')[1].split(' ')
         if o == 'outputformat':
             arg = c.get(section,o)
             if arg=='H':
-                outputFormat=0
+                g.outputFormat=0
             elif arg=='C':
-                outputFormat=1
+                g.outputFormat=1
             elif arg=='T':
-                outputFormat=2
+                g.outputFormat=2
 
     getParameters(paramlst)
 
 #Main
 #We should probably split this up but whatever
 def main():
-    global outputFormat
-    global filters
-    global outputDir, inputDir
-    global start_time, end_time
 
     parser = argparse.ArgumentParser(prog="""
                ,   .,---.,---.          |         
@@ -113,67 +108,67 @@ def main():
     parser.add_argument('--TSV', '-T', action='store_true', help='If you want a TSV report')
 
     args = parser.parse_args()
-    inputDir = helperFunctions.getFolderPath(args.inputDir)
+    g.inputDir = helperFunctions.getFolderPath(args.inputDir)
     if args.outputDir:
-        outputDir = helperFunctions.getFolderPath(args.outputDir)
+        g.outputDir = helperFunctions.getFolderPath(args.outputDir)
     if args.showParams:
-        possible_params = fileParser.checkFilesForParameters(inputDir)
-        for param in possible_params:
+        g.possible_params = fileParser.checkFilesForParameters(g.inputDir)
+        for param in g.possible_params:
             print(param.replace("-", " "))
     if args.paramlst:
         paramlst = getFilters(args.paramlst)
         getParameters(paramlst)
     if args.time:
-        parameters['Timestamp']=''
-        start_time,end_time=args.time.split(',')[0].split(' '),args.time.split(',')[1].split(' ')
+        g.parameters['Timestamp']=''
+        g.start_time,g.end_time=args.time.split(',')[0].split(' '),args.time.split(',')[1].split(' ')
     if args.configFile:
         loadConf(args.configFile)
     if args.HTML:
-        outputFormat='html'
+        g.outputFormat='html'
     elif args.CSV:
-        outputFormat='csv'
+        g.outputFormat='csv'
     elif args.TSV:
-        outputFormat='tsv'
+        g.outputFormat='tsv'
 
     if args.paramlst:
         getParameters(paramlst)
-        if len(parameters) > 0:
-            values, count = fileParser.parseFiles(inputDir, parameters)
+        if len(g.parameters) > 0:
+            g.values, g.count = fileParser.parseFiles(g.inputDir, g.parameters)
             #stupid way to check if -t
-            if 'Timestamp' in parameters:
+            if 'Timestamp' in g.parameters:
                 #take out everything except events in specified time range
                 tempv=[]
-                for v in values:
-                    date = v[list(parameters.keys()).index("Timestamp")]
+                for v in g.values:
+                    date = v[list(g.parameters.keys()).index("Timestamp")]
                     dt = datetime.datetime(int(date.split('/')[2].split(" ")[0]),int(date.split('/')[0]),int(date.split('/')[1]),int(date.split(' ')[1].split(':')[0]),int(date.split(':')[1]))
-                    if helperFunctions.checkDateinRange(start_time,end_time,dt):
+                    if helperFunctions.checkDateinRange(g.start_time,g.end_time,dt):
                         tempv.append(v)
-                values=tempv
-                for v in values:
-                    v.remove(v[list(parameters.keys()).index("Timestamp")])
-                del(parameters['Timestamp'])
+                g.values=tempv
+                for v in g.values:
+                    v.remove(v[list(g.parameters.keys()).index("Timestamp")])
+                del(g.parameters['Timestamp'])
                 temp = []
-                for v in values:
+                for v in g.values:
                     if v in temp:
-                        count[temp.index(v)] += 1
+                        g.count[temp.index(v)] += 1
                     else:
                         temp.append(v)
-                        count.append(1)
-                values = temp
+                        g.count.append(1)
+                g.values = temp
                     
             #Generating the reports
             
-            if outputFormat == 'html':
+            if g.outputFormat == 'html':
                 #If there wasn't a specified outputDir we just use the default(cwd)
-                if outputDir == '':
-                    htmlReportGen.generate(values, parameters, count)
+                if g.outputDir == '':
+                    htmlReportGen.generate(g.values, g.parameters, g.count)
                 else:
-                    htmlReportGen.generate(values, parameters, count, outputDir)
+                    htmlReportGen.generate(g.values, g.parameters, g.count, g.outputDir)
             else:
-                if outputDir == '':
-                    helperFunctions.genReport(values,parameters,count,repType=outputFormat)
+                if g.outputDir == '':
+                    helperFunctions.genReport(g.values,g.parameters,g.count,repType=g.outputFormat)
                 else:
-                    helperFunctions.genReport(values, parameters, count, outputDir, outputFormat)
+                    helperFunctions.genReport(g.values, g.parameters, g.count, g.outputDir, g.outputFormat)
 
         elif ('-P', '') not in opts and ('-h', '') not in opts:
             print(helpfile)
